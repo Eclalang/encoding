01d2146bf1c13abce5f800080e27ca143bb2f158
Axou89 <senecal.axel89@gmail.com>
2023-02-08 11:31:05 +0100

    add comments for function

--- encoding.go
+++ encoding.go
@@ -1,5 +1,6 @@
 package encoding
 
+// AsciiToString converts an array of ASCII values to a string
 func AsciiToString(intArr []int) string {
 	str := ""
 	for i := 0; i < len(intArr); i++ {
@@ -11,6 +12,7 @@ func AsciiToString(intArr []int) string {
 	return str
 }
 
+// StringToAscii converts a string to an array of ASCII values
 func StringToAscii(str string) []int {
 	intArr := make([]int, len(str))
 	for i := 0; i < len(str); i++ {
